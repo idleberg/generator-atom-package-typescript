e3505a76f22ea2f7b6aca5fb162ff5715cbe6fda
idleberg <git@idleberg.com>
2019-03-24 23:14:39 +0100

    fix function call

--- generators/app/index.js
+++ generators/app/index.js
@@ -94,7 +94,7 @@ module.exports = class extends Generator {
       {
         name: 'author',
         message: 'What\'s your GitHub username?',
-        default: async () => await this.github.username(),
+        default: async () => await this.user.github.username(),
         store: true,
         validate: x => x.length > 0 ? true : 'You have to provide a username',
         when: () => !this.options.org
